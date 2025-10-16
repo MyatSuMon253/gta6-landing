75bab0ad8415e545c18be57ed3fdec8de40f1507
MyatSuMon253 <myatsumon2531999@gmail.com>
2025-10-16 23:59:54 +0630

    feat: implement lucia character section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { ScrollTrigger } from "gsap/all";
 import FirstVideo from "./sections/FirstVideo";
 import Hero from "./sections/Hero";
 import Jason from "./sections/Jason";
+import Lucia from "./sections/Lucia";
 import Navbar from "./sections/Navbar";
 import SecondVideo from "./sections/SecondVideo";
 
@@ -16,6 +17,7 @@ const App = () => {
       <FirstVideo />
       <Jason />
       <SecondVideo />
+      <Lucia />
     </main>
   );
 };
